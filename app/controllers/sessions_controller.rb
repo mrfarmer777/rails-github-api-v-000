e0d37b9d27d71dde3ede2156151f28cc54d14e7d
Matt Farmer <mrfarmer777@gmail.com>
2018-08-01 22:45:04 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user
+  
   def create
+    code=params["code"]
+    
+    resp=Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      client_id=ENV["GITHUB_CLIENT_ID"]
+      client_secret=ENV["GITHUB_CLIENT_SECRET"]
+      req.body="{'client_id': #{client_id}, 'client_secret':#{client_secret}, 'code': #{code}}"
+    end
+    
+    
+    session[:token]=resp["access_token"]
+    redirect_to root_path
   end
 end
